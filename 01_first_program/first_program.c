c10eacd31f1f50d156d639b0320a8aea017e23a8
tpasc-poly-gh <tpaschel8929@floridapoly.edu>
2025-01-16 16:25:26 +0000

    create first program

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// One line comment
+
+/*
+    Multi Line Comment
+*/
+
+// Create Main Function
+int main() {
+    printf("Hello\norld!\n");
+    printf("I\nwant\nto\ncode!\n");
+    return 0;
+}
